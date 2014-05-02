6dbf4462d4a873b37e9d3ee872198d8bd36d68c4
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-05-02 17:47:44 -0500

    Removing version requirement on python cookbook

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures IPython Notebook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
 
-depends "python", "~> 1.3.5"
+depends "python"
 depends "yum", "~> 2.3.0"
 depends "apt", "~> 2.0.0"
 depends "supervisor", "~> 0.4.5"
